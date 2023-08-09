3d390d6b1e0c6742364b3103b97a0a3acec29ecd
Andrew Parra <nixonandrewparra@gmail.com>
2023-08-09 02:53:33 -0500

    Respnosive

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -19,15 +19,15 @@ const Weather = ({ weatherInfo }) => {
     ? `${kelvinToCelsius(temUrl)} ° C`
     : ` ${kelvinToFahrenheit(temUrl)}  °F`;
   return (
-    <section className="text-center  ">
+    <section className="text-center   ">
       <h2 className="text-3xl text-center m-3">{weatherInfo?.name}</h2>
-      <section className="grid  gap-4 sm:grid-cols-[auto_auto]">
+      <section className="grid  gap-3 sm:grid-cols-[350px_auto]  ">
         {/* seccion superior */}
-        <section className="bg-white/30 p-2 py-4 rounded-3xl grid gap-2 items-center align-center justify-center  ">
-          <h4 className="text-center text-3xl col-span-2 mt-2">
+        <section className="bg-white/30 p-4 py-4 rounded-3xl grid gap-2 items-center align-center justify-center   ">
+          <h4 className="text-center text-2xl col-span-2 mt-2 sm:text-[2rem] ">
             {weatherInfo?.weather[0].description}
           </h4>
-          <span className="text-6xl text-center  ">
+          <span className="text-[46px] text-center  sm:text-[3.7rem] ">
             {resultTempConversion}{" "}
           </span>
           <div className="w-[90px] ">
@@ -38,32 +38,34 @@ const Weather = ({ weatherInfo }) => {
           </div>
         </section>
         {/*seccion inferior  */}
-        <section className="bg-white/60  p-2 py-4 rounded-2xl  grid grid-cols-3 items-center align-center justify-center sm:grid-cols-1 ">
-          <article className="flex gap-2 items-center  ">
-            <div className=" w-[20px]">
+        <section className="bg-white/30 text-[0.8rem] font-bold scale-y-110 mb-3 divide-x-2 py-2 divide-[#0000003B]  rounded-3xl  grid grid-cols-[1fr,1fr,1fr] sm:grid-cols-1 sm:p-3 sm:py-3 sm:scale-y-100 sm:w-[auto] sm:text-center sm:divide-x-0 sm:divide-y-2 sm:font-black sm:text-[1rem] sm:mb-0">
+          <article className="flex   mr-4 items-center justify-center  py-1 px-1 sm:py-0 sm:justify-start sm:p-0 sm:mr-0 sm:px-0">
+            <div className=" w-[1.15rem] ml-4 sm:w-[1.40rem] sm:ml-0 ">
               <img src={"/images/wind.png"} alt="" />
             </div>
-            <span>{weatherInfo?.wind.speed}m/s</span>
+            <span className="ml-2 sm:ml-2">{weatherInfo?.wind.speed}m/s</span>
           </article>
 
-          <article className="flex gap-2 items-center align-center  ">
-            <div className=" w-[20px]">
+          <article className="flex items-center  justify-center sm:justify-start ">
+            <div className=" w-[1.20rem] ml-1 sm:w-6 sm:ml-0 ">
               <img src={"/images/humidity.png"} alt="" />
             </div>
-            <span>{weatherInfo?.main.humidity}%</span>
+            <span className="ml-2 sm:ml-4 ">{weatherInfo?.main.humidity}%</span>
           </article>
 
-          <article className="flex gap-2 items-center mr-2">
-            <div className=" w-[20px]">
+          <article className="flex  items-center  justify-center  sm:justify-start">
+            <div className=" w-[1.20rem] sm:w-[1.05rem">
               <img src={"/images/pressure.png"} alt="" />
             </div>
-            <span>{weatherInfo?.main.pressure} hPa</span>
+            <span className="ml-2 sm:ml-1 ">
+              {weatherInfo?.main.pressure} hPa
+            </span>
           </article>
         </section>
       </section>
       <button
         onClick={handleChangeUnitTemp}
-        className="bg-white text-blue-700 mt-3 rounded-3xl px-3 text-xl w-[151px] h-[34px] shadow-lg shadow-slate-500/50"
+        className="bg-white text-blue-700 mt-9 rounded-3xl px-3 text-xl w-[151px] h-[34px] shadow-lg shadow-slate-500/50  "
       >
         {isCelsius ? "switch to °F" : "switch to °C"}
       </button>
